a4ff1e4413311ca6b29066382a579cd032735f28
ohadBrunner <ohad272@gmail.com>
2021-09-10 15:52:47 +0300

    Design completed
    Added some js for the first question.

---
+++ index.js
@@ -0,0 +1,16 @@
+$(".q1").click(function() {
+  $(".a1").removeClass("answer-text");
+  $(".a1").addClass("visible");
+  $(".arrow").addClass("backwards-arrow");
+  $(".backwards-arrow").removeClass("arrow");
+});
+
+// $(".question-text").click(function(event) {
+//   switch(event.classList[1]) {
+//     case 1:
+//
+//   }
+// });
+
+
+// Complete before uploading to github.........
